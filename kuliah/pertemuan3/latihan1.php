dff02f0ad6b85819695dd2d19c851ca038827d52
Nndaaa404 <anandaarizkymaulana@gmail.com>
2024-02-20 12:08:02 +0700

    pertemuan 3

---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,17 @@
+<?php 
+$nama = "Nndaaa404";
+    echo "<h1>Hello $nama!</h1>";
+?>
+
+<?php 
+echo "Hello World!";
+?>
+
+<?php 
+$nama = 404;
+    echo "<h1>Error $nama!</h1>";
+?>
+
+<?php 
+echo 233040058;
+?>
---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,7 @@
+<?php 
+$nama = "Nndaaa404";
+$kampus = "Universitas Pasundan";
+$umur = 18;
+
+echo "Hello, nama saya $nama, umur saya $umur tahun, saya kuliah di $kampus.";
+?>
